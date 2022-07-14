cd097383398f404c77f78c796c333a5ff08f646a
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2022-07-14 19:33:15 +0900

    Updates

---
+++ precache-manifest.935fd1fa249d8a4a05c6d6d97b25c697.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ace95f630234841a53a6",
+    "url": "/static/css/main.8ae5be5a.chunk.css"
+  },
+  {
+    "revision": "ace95f630234841a53a6",
+    "url": "/static/js/main.ace95f63.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9255ed00f68afe581fc724eb6535465d",
+    "url": "/index.html"
+  }
+];
