73641995c858d07b7c1385aa76e087497e96245c
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-12-19 17:26:42 +0900

    fetch axios로 변경

--- src/api.ts
+++ src/api.ts
@@ -1,23 +1,35 @@
+import axios from "axios";
+
 const BASE_URL = "https://api.coinpaprika.com/v1";
 
 export let fetchCoins = async () => {
-  return fetch(`${BASE_URL}/coins`).then((response) => response.json());
+  return await axios
+    .get(`${BASE_URL}/coins`)
+    .then((res) => res.data)
+    .catch((err) => console.log(err));
 };
 
 export let fetchCoinInfo = async (coinId: string) => {
-  return fetch(`${BASE_URL}/coins/${coinId}`).then((response) =>
-    response.json()
-  );
+  return await axios
+    .get(`${BASE_URL}/coins/${coinId}`)
+    .then((res) => res.data)
+    .catch((err) => console.log(err));
 };
 
 export let fetchCoinTicker = async (coinId?: string) => {
-  return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
-    response.json()
-  );
+  return await axios
+    .get(`${BASE_URL}/tickers/${coinId}`)
+    .then((res) => res.data)
+    .catch((err) => console.log(err));
 };
 
-export let fetchCoinHistory = async (coinId? : string)=>{
-    const endDate = Math.floor(Date.now() /1000);
-    const startDate = endDate - 60 * 60 * 23;
-    return fetch(`${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`).then((response)=>response.json());
-}
+export let fetchCoinHistory = async (coinId?: string) => {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 60 * 60 * 23;
+  return await axios
+    .get(
+      `${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
+    )
+    .then((res) => res.data)
+    .catch((err) => console.log(err));
+};
