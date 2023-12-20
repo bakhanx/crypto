aaebaf932346397a3f4f4a89687cc8e672fe82db
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-12-20 22:51:21 +0900

    테마 수정

--- src/theme.ts
+++ src/theme.ts
@@ -1,20 +1,22 @@
-import {DefaultTheme} from 'styled-components';
+import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-    bgColor: "#1a1a1a",
-    textColor: "#c8d6e5",
-    accentColor: "#0abde3",
-    themeColor : "#0d0d0d",
-    chartColor : "dark",
-    darkModeColor : "yellow",
-    listBgColor : "black",
-  };
-  export const lightTheme: DefaultTheme = {
-    bgColor: "whitesmoke",
-    textColor: "black",
-    accentColor: "#0abde3",
-    themeColor : "#dfe6e9",
-    chartColor : "light",
-    darkModeColor : "orange",
-    listBgColor : "white"
-  };
+  bgColor: "#1a1a1a",
+  textColor: "#c8d6e5",
+  accentColor: "#0abde3",
+  themeColor: "#0d0d0d",
+  chartColor: "dark",
+  darkModeColor: "yellow",
+  listBgColor: "black",
+  overviewColor: "#0f1f3d",
+};
+export const lightTheme: DefaultTheme = {
+  bgColor: "whitesmoke",
+  textColor: "black",
+  accentColor: "#0abde3",
+  themeColor: "#336699",
+  chartColor: "light",
+  darkModeColor: "orange",
+  listBgColor: "white",
+  overviewColor: "#c6d8eb",
+};
