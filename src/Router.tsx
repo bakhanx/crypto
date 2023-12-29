74c1f401389bdbe1238161409a98dd0a50a2dda6
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-12-29 12:57:39 +0900

    github page 새로고침 버그 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import { Coin } from "./routes/Coin";
 import { Coins } from "./routes/Coins";
 import React from "react";
@@ -6,12 +6,14 @@ import { TopNav } from "./component/TopNav";
 
 export const Router = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-     <TopNav />
+    // <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
+      <TopNav />
       <Routes>
         <Route path="/" element={<Coins />} />
         <Route path="/:coinId/*" element={<Coin />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
+    // </BrowserRouter>
   );
 };
